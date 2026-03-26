52f215faa98838ba0d409b07b83df5ae41fc2be1
Pr0pyh <andrej.drmanovic@gmail.com>
2026-03-26 17:41:47 +0100

    novi level, skljoc zvuk

--- scenes/PhotoScan/photo_scan.gd
+++ scenes/PhotoScan/photo_scan.gd
@@ -2,6 +2,8 @@ class_name PhotoScanner
 extends Node3D
 
 @onready var scan_raycast: RayCast3D = $ScanRaycast
+@onready var anim_player: AnimationPlayer = $AnimationPlayer
+@onready var audio_player: AudioStreamPlayer = $AudioStreamPlayer
 @export var crosshair: TextureRect
 @export var photo_counter: PhotoCounter
 
@@ -15,3 +17,7 @@ func _process(delta: float):
 			if Input.is_action_just_pressed("take_photo"):
 				collider._on_shot()
 				photo_counter.add_scanned_photo()
+				anim_player.stop()
+				anim_player.play("reset")
+				anim_player.play("photo")
+				audio_player.play()
