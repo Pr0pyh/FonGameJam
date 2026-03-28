3e70b50b6da85198bf6bc0696569c0c6680aa446
Pr0pyh <andrej.drmanovic@gmail.com>
2026-03-28 19:56:26 +0100

    win screen bolestina ceo ekran

--- scenes/WinScreen/win_screen.gd
+++ scenes/WinScreen/win_screen.gd
@@ -1,4 +1,4 @@
-extends Control
+extends Window
 
 @export var win_counter = 10
 @export var phone_counter: PhotoCounter
@@ -6,9 +6,12 @@ extends Control
 
 func _ready() -> void:
 	anim_player.animation_finished.connect(destroy)
+	size = DisplayServer.screen_get_usable_rect().size
+	position = DisplayServer.screen_get_usable_rect().position
 
 func _process(delta: float) -> void:
 	if phone_counter.counter >= win_counter:
+		get_tree().paused = true
 		anim_player.play("winanim")
 
 func destroy(anim_name):
